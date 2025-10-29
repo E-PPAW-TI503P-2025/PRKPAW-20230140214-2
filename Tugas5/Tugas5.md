8f26388a600db3c2e278f5b4adaced8d474b5431
Andikarya17 <andikaryaputra17@gmail.com>
2025-10-29 09:55:27 +0700

    tugas baru

---
+++ Tugas5/Tugas5.md
@@ -0,0 +1,14 @@
+ Endpoint update data presensi
+![alt text](<Screenshot (90).png>)
+
+ Endpoint update jika format tanggal yang diisi tidak valid
+ ![alt text](<Screenshot (91).png>)
+
+ o	Endpoint delete data
+![alt text](<Screenshot (89).png>)
+
+o	Enpoint search berdasarkan nama
+![alt text](<Screenshot (92).png>)
+
+o	Endpoint search berdasarkan tanggal
+![alt text](<Screenshot (96).png>)
--- controllers/reportController.js
+++ controllers/reportController.js
@@ -2,30 +2,42 @@ const { Presensi } = require("../models");
 const { Op } = require("sequelize");
 const { format } = require("date-fns-tz");
 
+
+
 const timeZone = "Asia/Jakarta";
 
 exports.getDailyReport = async (req, res) => {
   try {
     console.log("Controller: Mengambil data laporan harian dari database...");
 
+    const { nama } = req.query; 
     const today = new Date();
     const formattedDate = format(today, "dd/MM/yyyy", { timeZone });
 
-    // Batas waktu hari ini (00:00 - 23:59)
+
     const startOfDay = new Date(today.setHours(0, 0, 0, 0));
     const endOfDay = new Date(today.setHours(23, 59, 59, 999));
 
-    // Ambil data dari tabel presensi
-    const dataPresensi = await Presensi.findAll({
-      where: {
-        checkIn: {
-          [Op.between]: [startOfDay, endOfDay],
-        },
+
+    const whereCondition = {
+      checkIn: {
+        [Op.between]: [startOfDay, endOfDay],
       },
+    };
+
+
+    if (nama) {
+      whereCondition.nama = {
+        [Op.like]: `%${nama}%`,
+      };
+    }
+
+
+    const dataPresensi = await Presensi.findAll({
+      where: whereCondition,
       order: [["checkIn", "ASC"]],
     });
 
-    // Format hasil agar lebih rapi
     const result = dataPresensi.map((item) => ({
       userId: item.userId,
       nama: item.nama,
